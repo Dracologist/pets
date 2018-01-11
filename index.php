525500616f0be65e994f4a5f287cbc513086319d
Dracologist <ejckanzler@yahoo.com>
2018-01-11 17:56:52 -0500

    added veiws

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
  */
 require_once('vendor/autoload.php');
 $f3 = Base::instance();
-->route('GET /', function() {
-        echo '<h1>Pets Home</h1>';
+    $f3->route('GET /', function() {
+        $view = new View;    echo $view->render
+        ('views/home.html');
     });
 $f3->run();
 ?>
